121e12dedf3b1e45baf1c5c83f7e375d4fcdd694
Silver <edgar.valmana@icloud.com>
2026-04-20 10:01:23 -0600

    Initial Commit

---
+++ 7/_App.swift
@@ -0,0 +1,17 @@
+//
+//  _App.swift
+//  7
+//
+//  Created by TEC on 20/04/26.
+//
+
+import SwiftUI
+
+@main
+struct _App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
